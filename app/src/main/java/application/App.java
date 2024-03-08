a0179612149324b911988aa92feb8f2ae404a290
EstefaniFelix <estefaniescola1@gmail.com>
2024-03-08 12:27:53 +0000

    save

--- app/src/main/java/application/App.java
+++ app/src/main/java/application/App.java
@@ -1,7 +1,11 @@
 package application;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+//@______ é uma dependencia, anotação
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!!!");
+        SpringApplication.run(App.class, args);
     }
 }
